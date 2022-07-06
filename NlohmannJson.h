3ee892d40c41e64e6f07558937b95b1329f60a99
liangzc <461380857@qq.com>
2022-07-06 16:31:07 +0800

    feat:增加NlohmannJson

---
+++ NlohmannJson.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+
+#include "src/NlJson.hpp"
+
+//using namespace ArduinoJson;
+
+#else
+
+#error ArduinoJson requires a C++ compiler, please change file extension to .cc or .cpp
+
+#endif
